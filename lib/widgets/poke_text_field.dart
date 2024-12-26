d112effb5237a33a72b4dbca1f304c87c9cc1254
XateroX <dan.lostarkgames@gmail.com>
2024-12-26 00:10:44 +0000

    tweak: remove focus node logic

--- lib/widgets/poke_text_field.dart
+++ lib/widgets/poke_text_field.dart
@@ -4,7 +4,7 @@ import 'package:pokemon_api_tool/api/pokemontcgio_response_cardstructure.dart';
 import 'package:tuple/tuple.dart';
 
 class PokeTextField extends StatelessWidget {
-  final FocusNode _textFocusNode = FocusNode();
+  // final FocusNode _textFocusNode = FocusNode();
   final Function(String) setFutureCardCallback;
   final TextEditingController textController;
 
@@ -15,21 +15,21 @@ class PokeTextField extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  void initState() {
-    _textFocusNode.requestFocus();
-  }
+  // void initState() {
+  //   _textFocusNode.requestFocus();
+  // }
 
   @override
-  void dispose() {
-    _textFocusNode.dispose();
-  }
+  // void dispose() {
+  //   _textFocusNode.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextField(
         controller: textController,
-        focusNode: _textFocusNode,
+        // focusNode: _textFocusNode,
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           hintText: 'Enter a Pokemon name or number (XXX/YYY)',
